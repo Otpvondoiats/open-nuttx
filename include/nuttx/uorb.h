2e693bb8366c9d167006475aefe038ca6ce95952
dongjiuzhu1 <dongjiuzhu1@xiaomi.com>
2025-01-13 23:25:08 +0800

    drivers/sensors: support set_nonwakeup ops for sensor driver to save power
    Signed-off-by: dongjiuzhu1 <dongjiuzhu1@xiaomi.com>

--- include/nuttx/uorb.h
+++ include/nuttx/uorb.h
@@ -1131,6 +1131,7 @@ struct sensor_state_s
   uint32_t nsubscribers;       /* The number of subcribers */
   uint32_t nadvertisers;       /* The number of advertisers */
   uint32_t generation;         /* The recent generation of circular buffer */
+  uint32_t nonwakeup;          /* The non wakeup state of sensor device */
   uint64_t priv;               /* The pointer to private data of userspace user */
 };
 
@@ -1141,7 +1142,8 @@ struct sensor_ustate_s
   uint32_t esize;              /* The element size of circular buffer */
   uint32_t latency;            /* The batch latency for user, in us */
   uint32_t interval;           /* The subscription interval for user, in us */
-  uint64_t generation;         /* The recent generation of circular buffer */
+  uint32_t nonwakeup;          /* The non wakeup state of sensor user */
+  uint32_t generation;         /* The recent generation of circular buffer */
 };
 
 /* This structure describes the register info for the user sensor */
